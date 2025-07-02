0727d45503ba14b5e868f7354df5c0dd0f658450
Fagner Menezes <fagnertavaresdemenezes2024@gmail.com>
2025-07-02 15:18:47 -0300

    primeiro commit

---
+++ src/main/java/com/fagnerdev/wefit_api_desafio/WefitApiDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.fagnerdev.wefit_api_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefitApiDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WefitApiDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fagnerdev/wefit_api_desafio/WefitApiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fagnerdev.wefit_api_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefitApiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
