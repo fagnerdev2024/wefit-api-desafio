8e028c4c39ea3cd97f2b642f124983b7f037fedc
Fagner Menezes <fagnertavaresdemenezes2024@gmail.com>
2025-07-02 15:26:04 -0300

    criacao entidade

---
+++ src/main/java/com/fagnerdev/wefit_api_desafio/model/PessoaJuridica.java
@@ -0,0 +1,40 @@
+package com.fagnerdev.wefit_api_desafio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Getter
+@Setter
+@Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class PessoaJuridica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cnpj;
+    private String cpfResponsavel;
+    private String nome;
+    private String celular;
+    private String telefone;
+    private String email;
+    private String confirmarEmail;
+
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String bairro;
+    private String estado;
+
+    private Boolean termos;
+}
