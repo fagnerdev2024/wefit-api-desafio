21cffcbbdd9649e8009d0a7750b2bd7dbf2a7e8a
Fagner Menezes <fagnertavaresdemenezes2024@gmail.com>
2025-07-02 23:12:52 -0300

    validacao cnpj e cpf

--- src/main/java/com/fagnerdev/wefit_api_desafio/service/PessoaJuridicaService.java
+++ src/main/java/com/fagnerdev/wefit_api_desafio/service/PessoaJuridicaService.java
@@ -4,6 +4,7 @@ package com.fagnerdev.wefit_api_desafio.service;
 import com.fagnerdev.wefit_api_desafio.dto.PessoaJuridicaDTO;
 import com.fagnerdev.wefit_api_desafio.model.PessoaJuridica;
 import com.fagnerdev.wefit_api_desafio.repository.PessoaJuridicaRepository;
+import com.fagnerdev.wefit_api_desafio.validacoes.PessoaJuridicaValidator;
 import jakarta.transaction.Transactional;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -12,45 +13,41 @@ import org.springframework.stereotype.Service;
 public class PessoaJuridicaService {
 
     private final PessoaJuridicaRepository pessoaJuridicaRepository;
+    private final PessoaJuridicaValidator pessoaJuridicaValidator;
 
-    public PessoaJuridicaService(PessoaJuridicaRepository pessoaJuridicaRepository) {
+    public PessoaJuridicaService(PessoaJuridicaRepository pessoaJuridicaRepository, PessoaJuridicaValidator pessoaJuridicaValidator) {
         this.pessoaJuridicaRepository = pessoaJuridicaRepository;
+        this.pessoaJuridicaValidator = pessoaJuridicaValidator;
     }
 
     @Transactional
-    public PessoaJuridica salvar(PessoaJuridicaDTO dto) {
-        if (!dto.email().equalsIgnoreCase(dto.confirmarEmail())) {
-            throw new IllegalArgumentException("Os campos 'email' e 'confirmarEmail' devem ser iguais.");
-        }
-
-        if (pessoaJuridicaRepository.existsByCnpj(dto.cnpj())) {
-            throw new IllegalArgumentException("CNPJ já cadastrado.");
-        }
+    public PessoaJuridica salvar(PessoaJuridicaDTO pessoaJuridicaDTO) {
+        pessoaJuridicaValidator.validarCadastro(pessoaJuridicaDTO);
 
         try {
-            PessoaJuridica entity = toEntity(dto);
-            return pessoaJuridicaRepository.save(entity);
+            PessoaJuridica pessoaJuridica = toEntity(pessoaJuridicaDTO);
+            return pessoaJuridicaRepository.save(pessoaJuridica);
         } catch (DataIntegrityViolationException e) {
             throw new RuntimeException("Erro ao salvar pessoa jurídica. Verifique os dados.", e);
         }
     }
 
-    private PessoaJuridica toEntity(PessoaJuridicaDTO dto) {
+    private PessoaJuridica toEntity(PessoaJuridicaDTO pessoaJuridicaDTO) {
         return PessoaJuridica.builder()
-                .cnpj(dto.cnpj())
-                .cpfResponsavel(dto.cpfResponsavel())
-                .nome(dto.nome())
-                .celular(dto.celular())
-                .telefone(dto.telefone())
-                .email(dto.email())
-                .cep(dto.cep())
-                .logradouro(dto.logradouro())
-                .numero(dto.numero())
-                .complemento(dto.complemento())
-                .cidade(dto.cidade())
-                .bairro(dto.bairro())
-                .estado(dto.estado())
-                .termosAceitos(dto.termosAceitos())
+                .cnpj(pessoaJuridicaDTO.cnpj())
+                .cpfResponsavel(pessoaJuridicaDTO.cpfResponsavel())
+                .nome(pessoaJuridicaDTO.nome())
+                .celular(pessoaJuridicaDTO.celular())
+                .telefone(pessoaJuridicaDTO.telefone())
+                .email(pessoaJuridicaDTO.email())
+                .cep(pessoaJuridicaDTO.cep())
+                .logradouro(pessoaJuridicaDTO.logradouro())
+                .numero(pessoaJuridicaDTO.numero())
+                .complemento(pessoaJuridicaDTO.complemento())
+                .cidade(pessoaJuridicaDTO.cidade())
+                .bairro(pessoaJuridicaDTO.bairro())
+                .estado(pessoaJuridicaDTO.estado())
+                .termosAceitos(pessoaJuridicaDTO.termosAceitos())
                 .build();
     }
 }
---
+++ src/main/java/com/fagnerdev/wefit_api_desafio/validacoes/PessoaJuridicaValidator.java
@@ -0,0 +1,52 @@
+package com.fagnerdev.wefit_api_desafio.validacoes;
+
+import com.fagnerdev.wefit_api_desafio.dto.PessoaJuridicaDTO;
+import com.fagnerdev.wefit_api_desafio.repository.PessoaJuridicaRepository;
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CNPJValidator;
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CPFValidator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PessoaJuridicaValidator {
+
+    private final PessoaJuridicaRepository pessoaJuridicaRepository;
+
+    public PessoaJuridicaValidator(PessoaJuridicaRepository pessoaJuridicaRepository) {
+        this.pessoaJuridicaRepository = pessoaJuridicaRepository;
+    }
+
+    public void validarCadastro(PessoaJuridicaDTO pessoaJuridicaDTO) {
+        validarEmailConfirmado(pessoaJuridicaDTO.email(), pessoaJuridicaDTO.confirmarEmail());
+        validarDuplicidadeCnpj(pessoaJuridicaDTO.cnpj());
+        validarCpf(pessoaJuridicaDTO.cpfResponsavel());
+        validarCnpj(pessoaJuridicaDTO.cnpj());
+    }
+
+    private void validarEmailConfirmado(String email, String confirmarEmail) {
+        if (!email.equalsIgnoreCase(confirmarEmail)) {
+            throw new IllegalArgumentException("Os campos 'email' e 'confirmarEmail' devem ser iguais.");
+        }
+    }
+
+    private void validarDuplicidadeCnpj(String cnpj) {
+        if (pessoaJuridicaRepository.existsByCnpj(cnpj)) {
+            throw new IllegalArgumentException("CNPJ já cadastrado.");
+        }
+    }
+
+    private void validarCnpj(String cnpj) {
+        CNPJValidator cnpjValidator = new CNPJValidator();
+        cnpjValidator.initialize(null);
+        if (!cnpjValidator.isValid(cnpj, null)) {
+            throw new IllegalArgumentException("CNPJ inválido.");
+        }
+    }
+
+    private void validarCpf(String cpf) {
+        CPFValidator cpfValidator = new CPFValidator();
+        cpfValidator.initialize(null);
+        if (!cpfValidator.isValid(cpf, null)) {
+            throw new IllegalArgumentException("CPF inválido.");
+        }
+    }
+}
