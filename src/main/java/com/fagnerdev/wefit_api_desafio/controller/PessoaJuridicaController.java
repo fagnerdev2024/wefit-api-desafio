7b54203ec731ee56167673112d3db0e9919f01d2
Fagner Menezes <fagnertavaresdemenezes2024@gmail.com>
2025-07-05 20:41:02 -0300

    incluindo injecao de dependencia via construtor

--- src/main/java/com/fagnerdev/wefit_api_desafio/controller/PessoaJuridicaController.java
+++ src/main/java/com/fagnerdev/wefit_api_desafio/controller/PessoaJuridicaController.java
@@ -1,12 +1,10 @@
 package com.fagnerdev.wefit_api_desafio.controller;
 
-
 import com.fagnerdev.wefit_api_desafio.config.PessoaJuridicaMapper;
 import com.fagnerdev.wefit_api_desafio.dto.PessoaJuridicaDTO;
 import com.fagnerdev.wefit_api_desafio.model.PessoaJuridica;
 import com.fagnerdev.wefit_api_desafio.service.PessoaJuridicaService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -17,14 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/pessoa-juridica")
 public class PessoaJuridicaController {
 
+    private final PessoaJuridicaService pessoaJuridicaService;
+    private final PessoaJuridicaMapper pessoaJuridicaMapper;
 
-    @Autowired
-    private PessoaJuridicaService pessoaJuridicaService;
-
-    @Autowired
-    private PessoaJuridicaMapper pessoaJuridicaMapper;
-
-
+    public PessoaJuridicaController(PessoaJuridicaService pessoaJuridicaService, PessoaJuridicaMapper pessoaJuridicaMapper) {
+        this.pessoaJuridicaService = pessoaJuridicaService;
+        this.pessoaJuridicaMapper = pessoaJuridicaMapper;
+    }
 
     @PostMapping
     public ResponseEntity<PessoaJuridicaDTO> cadastrar(@RequestBody @Valid PessoaJuridicaDTO pessoaJuridicaDTO) {
